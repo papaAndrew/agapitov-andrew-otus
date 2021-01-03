718295f0c9c1bb6aae3d5ef90f3fd6494c8940e2
papaAndrew <a.hermit71@gmail.com>
2021-01-03 20:35:05 +0500

    task 1 solved

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
-console.log("Hello world!");
+const myObject = {
+  name: 1,
+  items: [
+    {
+      name: 2,
+      items: [{ name: 3 }, { name: 4 }],
+    },
+    {
+      name: 5,
+      items: [{ name: 6 }],
+    },
+  ],
+};
+
+function buildTree(tree, prefix) {
+  const shift = typeof prefix === "undefined" ? "" : prefix;
+
+  tree.forEach((e, i) => {
+    const lastNode = i === tree.length - 1;
+
+    console.log(`${shift}${lastNode ? "└" : "├"} ${e.name}`);
+
+    if (e.items) {
+      buildTree(e.items, `${shift}${lastNode ? " " : "|"} `);
+    }
+  });
+}
+
+console.log(myObject.name);
+buildTree(myObject.items);
